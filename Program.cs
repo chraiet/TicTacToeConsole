8f1dd2448e3b709c1c6818acb4f94aceb869a630
Momo <chraiet009@outlook.com>
2020-01-10 11:33:53 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+            Game game = new Game();
+            game.PlayGame();
+            Console.WriteLine("Game Over");
+        }
+    }
+}
